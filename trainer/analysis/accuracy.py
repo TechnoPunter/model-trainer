554f2bb24d9823dd585b34967da7c451d72322f8
Pralhad Kamath <pralhad.kamath@pratishthanventures.com>
2023-12-02 22:07:56 +0530

    Added ability to load trade MTM data to DB.

--- trainer/analysis/accuracy.py
+++ trainer/analysis/accuracy.py
@@ -3,12 +3,9 @@ import os
 import pandas as pd
 from commons.backtest.fastBT import FastBT
 from commons.config.reader import cfg
+from commons.consts.consts import *
 from commons.dataprovider.database import DatabaseEngine
 from commons.loggers.setup_logger import setup_logging
-
-SUMMARY_PATH = os.path.join(cfg['generated'], 'summary')
-ACCURACY_FILE = os.path.join(SUMMARY_PATH, 'Portfolio-Accuracy.csv')
-TRADES_FILE = os.path.join(SUMMARY_PATH, 'Portfolio-Trades.csv')
 
 
 def run_accuracy(trader_db: DatabaseEngine):
@@ -22,9 +19,10 @@ def run_accuracy(trader_db: DatabaseEngine):
             raw_pred_df_ = pd.read_csv(file)
             params_.append({"scrip": scrip_, "strategy": strategy_, "raw_pred_df": raw_pred_df_})
 
-    bt_trades, bt_stats = f.run_accuracy(params_)
+    bt_trades, bt_stats, bt_mtm = f.run_accuracy(params_)
     bt_stats.to_csv(ACCURACY_FILE, float_format='%.2f', index=False)
     bt_trades.to_csv(TRADES_FILE, float_format='%.2f', index=False)
+    bt_mtm.to_csv(TRADES_MTM_FILE, float_format='%.2f', index=False)
     return bt_stats
 
 
