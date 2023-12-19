65e6830c119f5c920c5d01d583dabbca36698142
Pralhad Kamath <pralhad.kamath@pratishthanventures.com>
2023-12-19 14:29:50 +0530

    Adding ability to run BT for fixed num of days.

--- trainer/utils/TrainSplit.py
+++ trainer/utils/TrainSplit.py
@@ -1,4 +1,4 @@
-from datetime import datetime, timezone
+from datetime import datetime, timezone, timedelta
 
 import pandas as pd
 
@@ -16,6 +16,11 @@ def get_filtered_df(df: pd.DataFrame, cfg, type: str) -> (int, int):
         # Convert the datetime object to an epoch timestamp
         epoch_timestamp = int(date_obj.replace(tzinfo=timezone.utc).timestamp())
         filtered_df = df[df['time'] > epoch_timestamp]
+    elif filter_cfg.get('split-type') == "fixed_days":
+        num_days = filter_cfg['num-days']
+        start_date = datetime.today() - timedelta(days=num_days)
+        epoch_timestamp = int(start_date.replace(tzinfo=timezone.utc).timestamp())
+        filtered_df = df[df['time'] >= epoch_timestamp]
     else:
         total = len(df)
         runs = round(total * filter_cfg['split-pct'])
